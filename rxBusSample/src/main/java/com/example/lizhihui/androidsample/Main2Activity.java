50ed87e0dd68daa6719e89f07ffeb6cea0cd593f
ljf1172361058 <qwe123>
2016-11-12 20:50:38 +0800

    自己总结的最佳RxBus使用方式

--- rxBusSample/src/main/java/com/example/lizhihui/androidsample/Main2Activity.java
+++ rxBusSample/src/main/java/com/example/lizhihui/androidsample/Main2Activity.java
@@ -14,6 +14,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        
         findViewById(R.id.activity_main2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
